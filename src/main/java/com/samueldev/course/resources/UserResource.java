3f9b3c47f9273ee3673df796414c531f0afd2a03
user <samuelsouza.t@hotmail.com>
2023-08-14 18:26:34 -0300

    Service layer, component registration

--- src/main/java/com/samueldev/course/resources/UserResource.java
+++ src/main/java/com/samueldev/course/resources/UserResource.java
@@ -1,11 +1,16 @@
 package com.samueldev.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.samueldev.course.entities.User;
+import com.samueldev.course.services.UserService;
 
 // tells the program that this is an web resource using rest controllers
 @RestController 
@@ -13,18 +18,27 @@ import com.samueldev.course.entities.User;
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	/*
 	 *  end point to access the users
 	 * 	ResponseEntity is a spring type that returns responses from web requests
 	 *  @GetMapping indicates that this method will answer to GET requests from http
 	 */
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
 		/*
 		 * .ok() is used to send an success responses to http
 		 * .body() is the body of the response
 		 */
-		return ResponseEntity.ok().body(u);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return responseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/samueldev/course/services/UserService.java
@@ -0,0 +1,30 @@
+package com.samueldev.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.samueldev.course.entities.User;
+import com.samueldev.course.repositories.UserRepository;
+
+// Register this class as an spring component so it can be automatically injected with @Autowired
+// @Component
+
+// @Service does the same as @Component but its more specific
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
