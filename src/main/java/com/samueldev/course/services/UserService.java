a4c5246f1cfd348381b1779132b008daaed37d21
Samuel <samuelsouza.t@hotmail.com>
2023-08-16 20:56:20 -0300

    Exception handling - fundById

--- src/main/java/com/samueldev/course/services/UserService.java
+++ src/main/java/com/samueldev/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.samueldev.course.entities.User;
 import com.samueldev.course.repositories.UserRepository;
+import com.samueldev.course.services.exceptions.ResourceNotFoundException;
 
 // Register this class as an spring component so it can be automatically injected with @Autowired
 // @Component
@@ -25,7 +26,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id)); 
 	}
 	
 	public User insert(User obj) {
