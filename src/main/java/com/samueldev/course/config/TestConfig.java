3ada983d53fbe05cbea63d2590135c64d9ab3edd
Samuel <samuelsouza.t@hotmail.com>
2023-08-16 19:42:14 -0300

    Payment, one-to-one association

--- src/main/java/com/samueldev/course/config/TestConfig.java
+++ src/main/java/com/samueldev/course/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.samueldev.course.config;
 
+import java.nio.charset.CoderResult;
 import java.time.Instant;
 import java.util.Arrays;
 
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Profile;
 import com.samueldev.course.entities.Category;
 import com.samueldev.course.entities.Order;
 import com.samueldev.course.entities.OrderItem;
+import com.samueldev.course.entities.Payment;
 import com.samueldev.course.entities.Product;
 import com.samueldev.course.entities.User;
 import com.samueldev.course.entities.enums.OrderStatus;
@@ -84,5 +86,9 @@ public class TestConfig implements CommandLineRunner{
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
