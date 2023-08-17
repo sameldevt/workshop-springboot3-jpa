0b48a46e9553ccd63896d092d0e78b7960fd0aca
Samuel <samuelsouza.t@hotmail.com>
2023-08-16 21:08:40 -0300

    Exception handling - delete

--- src/main/java/com/samueldev/course/services/UserService.java
+++ src/main/java/com/samueldev/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.samueldev.course.entities.User;
 import com.samueldev.course.repositories.UserRepository;
+import com.samueldev.course.services.exceptions.DatabaseException;
 import com.samueldev.course.services.exceptions.ResourceNotFoundException;
 
 // Register this class as an spring component so it can be automatically injected with @Autowired
@@ -34,7 +37,15 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public User update(Long id, User obj) {
